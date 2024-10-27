ff42ad7dd38aa51224af7a863701a219ea34c16f
ohmprakashganesh <131688194+ohmprakashganesh@users.noreply.github.com>
2024-10-27 12:08:56 +0545

    login page

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+   const [email, setEmail] = useState("");
+   const [password, setPassword] = useState("");
+   console.log(email);
+   console.log(password);
+
+
+  const submitHandler=(e)=>{
+    e.preventDefault();
+    console.log("hello submit");
+
+  }
   return(
-    <div className='w-full h-screen flex items-center justify-center'>
-        <div className='border-2 border-red-400'>
-          <form className='flex flex-col items-center justify-center'>
-            <input className='border-2 border-r-emerald-500 py-3 px-5 rounded-full text-xl outline-none bg-transparent' type='email' placeholder='enter your email'/>
-            <input type='password' placeholder='enter your Password'/>
-            <button> Log in </button>
+    <div className='w-full h-screen text-center flex items-center justify-center'>
+        <div className='border-2 p-8  shadow-gray-600 shadow-lg   '>
+          <form 
+          onSubmit={(e)=>{ submitHandler(e)  }} className='flex text-center flex-col items-center justify-center'>
+            <input
+            required
+            value={email}
+            onChange={(e)=>{setEmail(e.target.value)}}
+             className='border-2 text-center border-emerald-600 py-3 px-5 mt-3 outline-none rounded-full text-xl  bg-transparent placeholder:text-black' type='email' placeholder='enter your email'/>
+            <input 
+            required
+            value={password}
+            onChange={(e)=> {setPassword(e.target.value)}}
+            type='password' className=' border-2 border-emerald-600  rounded-full text-xl outline-none px-5 py-3 mt-5' placeholder='enter your Password'/>
+            <button className='border-2 bg-green-500 text-2xl font-mono border-emerald-600 rounded-full text-xl outline-none px-5 py-3 mt-5'> Log in </button>
           </form>
         </div>
     </div>
